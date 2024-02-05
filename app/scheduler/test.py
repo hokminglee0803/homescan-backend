d322756b955a39b6e21dddc30f432db7748121c8
hokminglee0803 <59437566+hokminglee0803@users.noreply.github.com>
2024-02-05 22:00:05 +0800

    Update test.py

--- app/scheduler/test.py
+++ app/scheduler/test.py
@@ -13,7 +13,7 @@ class TestScraper:
     def scrape(self, selected_region, selected_district):
         chrome_options = Options()
         chrome_options.add_argument("--headless")  # 如果不需要顯示瀏覽器視窗，可以加上此行設定
-        driver = webdriver.Chrome(chrome_options=chrome_options)
+        driver = webdriver.Chrome(options=chrome_options)
         driver.get("https://www.hsbc.com.hk/zh-hk/mortgages/tools/property-valuation/")  # 替換為您想要打開的網址
         # 在這裡執行您的網頁操作
         time.sleep(5)  # 簡單示範等待 5 秒
