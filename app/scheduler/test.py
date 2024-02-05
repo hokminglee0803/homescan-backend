c69bb373d6c4426ecab69e42e31a6b98bc540ee2
hokminglee0803 <59437566+hokminglee0803@users.noreply.github.com>
2024-02-05 22:44:39 +0800

    Update test.py

--- app/scheduler/test.py
+++ app/scheduler/test.py
@@ -3,6 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
 import logging
 
 logger = logging.getLogger(__name__)
@@ -10,6 +11,12 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class TestScraper:
+
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument("--headless")  
+    broswer = webdriver.Chrome(options=chrome_options)
 
     def click_field(self, field_idx, id, broswer: webdriver.Chrome):
         retry = 1
@@ -25,17 +32,28 @@ class TestScraper:
                 time.sleep(2)
                 retry += 1
 
+    def retry_on_crash(self,func):
+        def wrapper(*args, **kwargs):
+            max_retries = 3
+            retries = 0
+            while retries < max_retries:
+                try:
+                    return func(*args, **kwargs)
+                except WebDriverException as e:
+                    print(f"Page crash occurred. Retrying... ({retries+1}/{max_retries})")
+                    self.broswer.refresh()
+                    retries += 1
+            raise Exception("Failed after multiple retries")
+        return wrapper
+
+    @retry_on_crash
     def scrape(self, selected_region, selected_district):
-        chrome_options = Options()
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument("--headless")  
-        broswer = webdriver.Chrome(options=chrome_options)
-        broswer.get("https://www.hsbc.com.hk/zh-hk/mortgages/tools/property-valuation/") 
-    
+
+        self.broswer.get("https://www.hsbc.com.hk/zh-hk/mortgages/tools/property-valuation/") 
+
         time.sleep(5)  
 
-        logger.info(broswer.title)
+        logger.info(self.broswer.title)
 
         # self.click_field(field_idx=selected_region, id=1,
         #                      broswer=broswer)
@@ -45,5 +63,5 @@ class TestScraper:
         
         # logger.info(selected_region)
 
-        broswer.quit()
+        self.broswer.quit()
        
