a197baa9d4092f7405b5373319fdebec4c25d93e
hokminglee0803 <59437566+hokminglee0803@users.noreply.github.com>
2024-02-05 22:47:45 +0800

    Update test.py

--- app/scheduler/test.py
+++ app/scheduler/test.py
@@ -3,7 +3,6 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import WebDriverException
 import logging
 
 logger = logging.getLogger(__name__)
@@ -12,48 +11,40 @@ logger = logging.getLogger(__name__)
 @dataclass
 class TestScraper:
 
-    chrome_options = Options()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument("--headless")  
-    broswer = webdriver.Chrome(options=chrome_options)
+    # def click_field(self, field_idx, id, broswer: webdriver.Chrome):
+    #     retry = 1
+    #     while retry < 5:
+    #         try:
+    #             broswer.find_element(
+    #                 by=By.ID, value=f"tools_form_{id}_selectized").click()
+    #             time.sleep(0.5)
+    #             broswer.find_element(by=By.ID, value=f"tools_form_{id}_menu").find_elements(
+    #                 by=By.TAG_NAME, value="div")[field_idx].click()
+    #             retry = 5
+    #         except Exception:
+    #             time.sleep(2)
+    #             retry += 1
 
-    def click_field(self, field_idx, id, broswer: webdriver.Chrome):
-        retry = 1
-        while retry < 5:
-            try:
-                broswer.find_element(
-                    by=By.ID, value=f"tools_form_{id}_selectized").click()
-                time.sleep(0.5)
-                broswer.find_element(by=By.ID, value=f"tools_form_{id}_menu").find_elements(
-                    by=By.TAG_NAME, value="div")[field_idx].click()
-                retry = 5
-            except Exception:
-                time.sleep(2)
-                retry += 1
-
-    def retry_on_crash(self,func):
-        def wrapper(*args, **kwargs):
-            max_retries = 3
-            retries = 0
-            while retries < max_retries:
-                try:
-                    return func(*args, **kwargs)
-                except WebDriverException as e:
-                    print(f"Page crash occurred. Retrying... ({retries+1}/{max_retries})")
-                    self.broswer.refresh()
-                    retries += 1
-            raise Exception("Failed after multiple retries")
-        return wrapper
-
-    @retry_on_crash
     def scrape(self, selected_region, selected_district):
+        chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument("--headless")  
+        broswer = webdriver.Chrome(options=chrome_options)
 
-        self.broswer.get("https://www.hsbc.com.hk/zh-hk/mortgages/tools/property-valuation/") 
-
+        max_retries = 10
+        retries = 0
+        while retries < max_retries:
+            try:
+                broswer.get("https://www.hsbc.com.hk/zh-hk/mortgages/tools/property-valuation/") 
+            except :
+                print(f"Page crash occurred. Retrying... ({retries+1}/{max_retries})")
+                broswer.refresh()
+                retries += 1
+                time.sleep(5)
+        
         time.sleep(5)  
-
-        logger.info(self.broswer.title)
+        logger.info(broswer.title)
 
         # self.click_field(field_idx=selected_region, id=1,
         #                      broswer=broswer)
@@ -63,5 +54,5 @@ class TestScraper:
         
         # logger.info(selected_region)
 
-        self.broswer.quit()
+        broswer.quit()
        
