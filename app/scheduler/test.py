ad942fa045b77a82fefeda6eeee4fa334a347b9b
hokminglee0803 <59437566+hokminglee0803@users.noreply.github.com>
2024-02-06 23:52:30 +0800

    Update test.py

--- app/scheduler/test.py
+++ app/scheduler/test.py
@@ -19,27 +19,27 @@ class TestScraper:
 
     broswer: webdriver.Chrome = None
 
-    region_selected_idx = 0
-    district_selected_idx = 0
-    estate_selected_idx = 0
-
-    def __init__(self):
-        self.open_browser()
-
-    def click_field(self, field_idx, id):
-        self.browser.find_element(
+    def click_field(self, field_idx, id, browser: webdriver.Chrome):
+        retry = 1
+        while retry < 10:
+            try:
+                browser.find_element(
                     by=By.ID, value=f"tools_form_{id}_selectized").click()
-        time.sleep(1)
-        self.browser.find_element(by=By.ID, value=f"tools_form_{id}_menu").find_elements(
+                time.sleep(1)
+                browser.find_element(by=By.ID, value=f"tools_form_{id}_menu").find_elements(
                     by=By.TAG_NAME, value="div")[field_idx].click()
-        time.sleep(1)
-        selected_text = self.browser.find_element(
-                    by=By.ID, value=f"tools_form_{id}_selected_text").text
-        if selected_text == None or selected_text == 'None':
-            raise ""
-        else:
-            return selected_text
-        
+                time.sleep(1)
+                selected_text = browser.find_element(
+                            by=By.ID, value=f"tools_form_{id}_selected_text").text
+                if selected_text == None or selected_text == 'None':
+                    raise Exception("Selected Text is Null")
+                else:
+                    return selected_text
+            except Exception:
+                time.sleep(30)
+                retry += 1
+        return ""
+
     def retry_on_crash(func):
         def wrapper(*args, **kwargs):
             max_retries = 20
@@ -48,10 +48,12 @@ class TestScraper:
                 try:
                     return func(*args, **kwargs)
                 except Exception as e:
-                    logger.warning(f"Page crash occurred. Retrying... ({retries+1}/{max_retries})")
+                    logger.warning(f"Something crash occurred. Retrying... ({retries+1}/{max_retries})")
+                    logger.warning(e)
                     retries += 1
                     time.sleep(random.uniform(30, 100))
             raise Exception("Failed after multiple retries")
+
         return wrapper
 
     @retry_on_crash
@@ -64,13 +66,14 @@ class TestScraper:
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument("--log-level=3")
-        self.browser = webdriver.Remote(
+        browser = webdriver.Remote(
             command_executor='http://18.141.147.24:4444/wd/hub',
             options=chrome_options
         )
-        self.browser.get("https://www.hsbc.com.hk/zh-hk/mortgages/tools/property-valuation/")
+        browser.get("https://www.hsbc.com.hk/zh-hk/mortgages/tools/property-valuation/")
         time.sleep(2)
-        logger.debug(self.browser.title)
+        logger.debug(browser.title)
+        return browser
 
     def scrape_estates(self,browser:webdriver.Chrome):
         estates_select = browser.find_element(
@@ -81,18 +84,25 @@ class TestScraper:
             by=By.ID, value="tools_form_3_menu").find_elements(by=By.TAG_NAME, value="div")
         estates_select.click()
 
-    @retry_on_crash
     def scrape(self, selected_region, selected_district):
-        region_selected = self.click_field(field_idx=selected_region, id=1)      
-        time.sleep(2)
-        district_selected = self.click_field(field_idx=selected_district, id=2)   
 
-        self.scrape_estates()
-        for estate_idx, estate in enumerate(self.estates):
-            if estate_idx > 0:
-                estate_selected = self.click_field(field_idx=estate_idx,id=3)
-                logger.info(f'{region_selected} - {district_selected} - {estate_selected}')
+        browser = self.open_browser()
 
+        try:
+            region_selected = self.click_field(field_idx=selected_region, id=1,
+                             browser=browser)      
+            time.sleep(2)
+            district_selected = self.click_field(field_idx=selected_district, id=2,
+                             browser=browser)   
+
+            self.scrape_estates()
+            for estate_idx, estate in enumerate(self.estates):
+                if estate_idx > 0:
+                    estate_selected = self.click_field(field_idx=estate_idx,id=3, browser=browser)
+
+                    logger.info(f'{region_selected} - {district_selected} - {estate_selected}')
+        finally:
+            browser.quit()
 
        
        
