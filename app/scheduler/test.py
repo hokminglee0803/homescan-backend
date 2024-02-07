cd4afec1785e9b3d1d530c153b23287eb03200d5
hokminglee0803 <59437566+hokminglee0803@users.noreply.github.com>
2024-02-07 16:27:47 +0800

    Update test.py

--- app/scheduler/test.py
+++ app/scheduler/test.py
@@ -7,15 +7,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import logging
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import concurrent.futures
 from app.services.house import HouseService
 
 from app.utils.mongodb import close_mongodb_connection, connect_to_mongodb
 
 logger = logging.getLogger(__name__)
 
-WAIT_TIMEOUT = 10
+
 @dataclass
 class TestScraper:
 
@@ -32,12 +31,14 @@ class TestScraper:
         retry = 1
         while retry < 10:
             try:
-                WebDriverWait(browser, WAIT_TIMEOUT).until(EC.presence_of_element_located((By.ID, f"tools_form_{id}_selectized"))).click()
-
-                WebDriverWait(browser, WAIT_TIMEOUT).until(EC.presence_of_element_located((By.ID, f"tools_form_{id}_menu"))).find_elements(
+                browser.find_element(
+                    by=By.ID, value=f"tools_form_{id}_selectized").click()
+                time.sleep(1)
+                browser.find_element(by=By.ID, value=f"tools_form_{id}_menu").find_elements(
                     by=By.TAG_NAME, value="div")[field_idx].click()
-                
-                selected_text = WebDriverWait(browser, WAIT_TIMEOUT).until(EC.presence_of_element_located((By.ID, f"tools_form_{id}_selected_text"))).text
+                time.sleep(1)
+                selected_text = browser.find_element(
+                            by=By.ID, value=f"tools_form_{id}_selected_text").text
                 if selected_text == None or selected_text == 'None':
                     raise Exception("Selected Text is Null")
                 else:
@@ -63,7 +64,7 @@ class TestScraper:
         return wrapper
 
     @retry_on_crash
-    async def open_browser(self):
+    def open_browser(self):
         chrome_options = Options()
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument("--headless")  
@@ -85,55 +86,63 @@ class TestScraper:
         return browser
 
     def scrape_estates(self,browser:webdriver.Chrome):
-        estates_select = WebDriverWait(browser, WAIT_TIMEOUT).until(EC.presence_of_element_located((By.ID, "tools_form_3_selectized")))
+        estates_select = browser.find_element(
+            by=By.ID, value="tools_form_3_selectized")
         estates_select.click()
         time.sleep(0.5)
-        self.estates  = WebDriverWait(browser, WAIT_TIMEOUT).until(EC.presence_of_element_located((By.ID, "tools_form_3_menu"))).find_elements(by=By.TAG_NAME, value="div")
+        self.estates = browser.find_element(
+            by=By.ID, value="tools_form_3_menu").find_elements(by=By.TAG_NAME, value="div")
         estates_select.click()
 
     def scrape_buldings(self,browser:webdriver.Chrome):
-        buildings_select = WebDriverWait(browser, WAIT_TIMEOUT).until(EC.presence_of_element_located((By.ID, "tools_form_4_selectized")))
+        buildings_select = browser.find_element(
+            by=By.ID, value="tools_form_4_selectized")
         buildings_select.click()
         time.sleep(0.5)
-        self.buildings = WebDriverWait(browser, WAIT_TIMEOUT).until(EC.presence_of_element_located((By.ID, "tools_form_4_menu"))).find_elements(by=By.TAG_NAME, value="div")
+        self.buildings = browser.find_element(
+            by=By.ID, value="tools_form_4_menu").find_elements(by=By.TAG_NAME, value="div")
         buildings_select.click()
 
     def scrape_floors(self,browser:webdriver.Chrome):
-        floors_select = WebDriverWait(browser, WAIT_TIMEOUT).until(EC.presence_of_element_located((By.ID, "tools_form_5_selectized")))
+        floors_select = browser.find_element(
+            by=By.ID, value="tools_form_5_selectized")
         floors_select.click()
         time.sleep(0.5)
-        self.floors = WebDriverWait(browser, WAIT_TIMEOUT).until(EC.presence_of_element_located((By.ID, "tools_form_5_menu"))).find_elements(by=By.TAG_NAME, value="div")
+        self.floors = browser.find_element(
+            by=By.ID, value="tools_form_5_menu").find_elements(by=By.TAG_NAME, value="div")
         floors_select.click()
 
     def scrape_blocks(self,browser:webdriver.Chrome):
-        blocks_select = WebDriverWait(browser, WAIT_TIMEOUT).until(EC.presence_of_element_located((By.ID, "tools_form_6_selectized")))
+        blocks_select = browser.find_element(
+            by=By.ID, value="tools_form_6_selectized")
         blocks_select.click()
         time.sleep(0.5)
-        self.blocks = WebDriverWait(browser, WAIT_TIMEOUT).until(EC.presence_of_element_located((By.ID, "tools_form_6_menu"))).find_elements(by=By.TAG_NAME, value="div")
+        self.blocks = browser.find_element(
+            by=By.ID, value="tools_form_6_menu").find_elements(by=By.TAG_NAME, value="div")
         blocks_select.click()
 
-    async def valuation(self,browser:webdriver.Chrome,region_selected,district_selected,estate_selected,building_selected,floor_selected,block_selected):
+    def valuation(self,browser:webdriver.Chrome,region_selected,district_selected,estate_selected,building_selected,floor_selected,block_selected):
         valuation = ""
         retry = 1
         while retry < 10:
             try:
-                submit_button = WebDriverWait(browser, WAIT_TIMEOUT).until(EC.presence_of_element_located((By.XPATH, '//*[@id="property-valuation-search"]/div[2]/form/div/div[2]/div[1]/div/div[7]/a')))
+                submit_button = browser.find_element(By.XPATH, value='//*[@id="property-valuation-search"]/div[2]/form/div/div[2]/div[1]/div/div[7]/a')
                 submit_button.click()
                 time.sleep(5)
-                valuation = WebDriverWait(browser, WAIT_TIMEOUT).until(EC.presence_of_element_located((By.XPATH, '//*[@id="property-valuation-search"]/div[2]/form/div/div[2]/div[2]/div/div[2]/div[2]/div[2]/span'))).text
+                valuation = browser.find_element(By.XPATH, value='//*[@id="property-valuation-search"]/div[2]/form/div/div[2]/div[2]/div/div[2]/div[2]/div[2]/span').text
                 if valuation == "":
                     raise Exception
                 else:
                     # browser.save_screenshot(f"{region_idx}-{district_idx}-{estate_idx}-{building_idx}-{floor_idx}-{block_idx}.png")
                     retry = 10
 
-                    gross_floor_area =WebDriverWait(browser, WAIT_TIMEOUT).until(EC.presence_of_element_located((By.XPATH, '//*[@id="property-valuation-search"]/div[2]/form/div/div[2]/div[2]/div/div[2]/div[3]/div[2]/span'))).text
-                    saleable_area = WebDriverWait(browser, WAIT_TIMEOUT).until(EC.presence_of_element_located((By.XPATH, '//*[@id="property-valuation-search"]/div[2]/form/div/div[2]/div[2]/div/div[2]/div[4]/div[2]/span'))).text
-                    property_age = WebDriverWait(browser, WAIT_TIMEOUT).until(EC.presence_of_element_located((By.XPATH, '//*[@id="property-valuation-search"]/div[2]/form/div/div[2]/div[2]/div/div[2]/div[5]/div[2]/span'))).text
+                    gross_floor_area = browser.find_element(By.XPATH, value='//*[@id="property-valuation-search"]/div[2]/form/div/div[2]/div[2]/div/div[2]/div[3]/div[2]/span').text
+                    saleable_area = browser.find_element(By.XPATH, value='//*[@id="property-valuation-search"]/div[2]/form/div/div[2]/div[2]/div/div[2]/div[4]/div[2]/span').text
+                    property_age = browser.find_element(By.XPATH, value='//*[@id="property-valuation-search"]/div[2]/form/div/div[2]/div[2]/div/div[2]/div[5]/div[2]/span').text
 
                     logger.info(f'{region_selected} - {district_selected} - {estate_selected} - {building_selected} - {floor_selected} - {block_selected}  --- Valuation: {valuation}')
 
-                    self.house_service.update_house_hsbc({
+                    asyncio.create_task(self.house_service.update_house_hsbc({
                         "valuation": valuation,
                         "region": region_selected,
                         "district": district_selected,
@@ -144,14 +153,14 @@ class TestScraper:
                         "gross_floor_area": gross_floor_area,
                         "saleable_area": saleable_area,
                         "property_age": property_age,
-                    })
+                    }))
             except:
                 retry += 1
                 time.sleep(10) 
 
     async def scrape(self, selected_region, selected_district):
         retry = 0
-        browser = await self.open_browser()
+        browser = self.open_browser()
         while retry<10:
             try:
                 region_selected = self.click_field(field_idx=selected_region, id=1,
@@ -182,7 +191,7 @@ class TestScraper:
                                             if block_idx > 0:
                                                 block_selected = self.click_field(
                                                             field_idx=block_idx, id=6, browser=browser)
-                                                await self.valuation(browser=browser,region_selected=region_selected, district_selected=district_selected, estate_selected=estate_selected, building_selected=building_selected,floor_selected=floor_selected,block_selected=block_selected)
+                                                self.valuation(browser=browser,region_selected=region_selected, district_selected=district_selected, estate_selected=estate_selected, building_selected=building_selected,floor_selected=floor_selected,block_selected=block_selected)
                 retry = 10
             except:
                 retry += 1
