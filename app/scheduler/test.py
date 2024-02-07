efb2e259fc19a4975bf90ebd200e374c4728c6fb
hokminglee0803 <59437566+hokminglee0803@users.noreply.github.com>
2024-02-07 14:14:14 +0800

    Update test.py

--- app/scheduler/test.py
+++ app/scheduler/test.py
@@ -61,7 +61,7 @@ class TestScraper:
                 except Exception as e:
                     logger.warning(f"Something crash occurred. Retrying... ({retries+1}/{max_retries})")
                     retries += 1
-                    time.sleep(random.uniform(10, 300))
+                    time.sleep(random.uniform(10, 20))
             raise Exception("Failed after multiple retries")
 
         return wrapper
