1b924d0abfbedd624cc5b98d6ea5217e23cbcad3
hokminglee0803 <59437566+hokminglee0803@users.noreply.github.com>
2024-02-08 11:09:59 +0800

    Update

--- app/scheduler/thread.py
+++ app/scheduler/thread.py
@@ -55,7 +55,7 @@ class ThreadScraper:
             try:
                 options = Options()
                 options.add_argument("--no-sandbox")
-                options.add_argument("--headless")
+                # options.add_argument("--headless")
                 options.add_argument("--disable-gpu")
                 options.add_argument('--disable-dev-shm-usage')
                 options.add_argument("--window-size=1920,1080")
