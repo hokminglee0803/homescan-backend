90faadaef83bf09649ac948ef8cf5581a14dbf85
hokminglee0803 <59437566+hokminglee0803@users.noreply.github.com>
2024-02-09 12:28:56 +0800

    Update test.py

--- app/scheduler/test.py
+++ app/scheduler/test.py
@@ -183,6 +183,7 @@ class TestScraper:
                                 browser=browser)   
 
                 self.scrape_estates(browser=browser)
+                logger.info(self.estates.reverse())
                 for estate_idx, estate in enumerate(self.estates.reverse()):
                     if estate_idx > 0:
                         estate_selected = self.click_field(field_idx=estate_idx,id=3, browser=browser)
