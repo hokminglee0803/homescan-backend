1488e9a26d57d3888cc329742c6ca21c6bb7f570
hokminglee0803 <59437566+hokminglee0803@users.noreply.github.com>
2024-02-15 14:07:44 +0800

    Update

--- app/scheduler/test.py
+++ app/scheduler/test.py
@@ -171,27 +171,23 @@ class TestScraper:
                                 browser=browser)   
 
                 self.scrape_estates(browser=browser)
-                random.shuffle(self.estates)
                 for estate_idx, estate in enumerate(self.estates):
                     if estate_idx > 0:
                         estate_selected = self.click_field(field_idx=estate_idx,id=3, browser=browser)
 
                         self.scrape_buldings(browser=browser)
-                        random.shuffle(self.buildings)
                         for building_idx, building in enumerate(self.buildings):
                             if building_idx > 0:
                                 building_selected = self.click_field(
                                         field_idx=building_idx, id=4, browser=browser)
 
                                 self.scrape_floors(browser=browser)
-                                random.shuffle(self.floors)
                                 for floor_idx, floor in enumerate(self.floors):
                                     if floor_idx > 0:
                                         floor_selected = self.click_field(
                                                 field_idx=floor_idx, id=5, browser=browser)
 
                                         self.scrape_blocks(browser=browser)
-                                        random.shuffle(self.blocks)
                                         for block_idx, block in enumerate(self.blocks):
                                             if block_idx > 0:
                                                 block_selected = self.click_field(
@@ -205,8 +201,6 @@ class TestScraper:
                 retry = 10
                 logger.info('Closing Browser')
                 browser.quit()
-                # close_mongodb_connection()
-                # logger.info('Close connection to Mongo DB.')
 
        
        
