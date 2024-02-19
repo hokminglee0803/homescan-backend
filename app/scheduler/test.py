a5a5901d65dc1eeb70cb9a215b74dd657456d1ba
leehokm <leehokm@mfcgd.com>
2024-02-19 11:16:38 +0800

    Update test.py

--- app/scheduler/test.py
+++ app/scheduler/test.py
@@ -24,6 +24,11 @@ class TestScraper:
     floors = []
     blocks = []
 
+    current_estates_idx = 10
+    current_buildings_idx = 1
+    current_floor_idx = 1
+    current_blocks_idx = 1
+
     house_service = HouseService()
 
     def __exit__(self):
@@ -222,7 +227,7 @@ class TestScraper:
         retry = 0
 
      
-        while retry<100:
+        while retry < 100:
             browser = self.open_browser()
             try:
                 region_selected = self.click_field(field_idx=selected_region, id=1,
@@ -233,23 +238,28 @@ class TestScraper:
 
                 self.scrape_estates(browser=browser)
                 for estate_idx, estate in enumerate(self.estates):
+                    if estate_idx > 0 and estate_idx >= self.current_estates_idx:
+                        self.current_estates_idx = estate_idx
                         estate_selected = self.click_field(field_idx=estate_idx,id=3, browser=browser)
                         self.scrape_buldings(browser=browser)
                         for building_idx, building in enumerate(self.buildings):
-                            if building_idx > 0:
+                            if building_idx > 0 and building_idx >= self.current_buildings_idx:
+                                self.current_buildings_idx = building_idx
                                 building_selected = self.click_field(
                                         field_idx=building_idx, id=4, browser=browser)
-
+                                
                                 self.scrape_floors(browser=browser)
                                 for floor_idx, floor in enumerate(self.floors):
-                                    if floor_idx > 0:
+                                    if floor_idx > 0 and floor_idx >= self.current_floor_idx:
+                                        self.current_floor_idx = floor_idx
                                         floor_selected = self.click_field(
                                                 field_idx=floor_idx, id=5, browser=browser)
 
                                         self.scrape_blocks(browser=browser)
                                         random.shuffle(self.blocks)
                                         for block_idx, block in enumerate(self.blocks):
-                                            if block_idx > 0:
+                                            if block_idx > 0 and block_idx >= self.current_blocks_idx:
+                                                self.current_blocks_idx = block_idx
                                                 if time.time() - browser.start_time >= 1800:
                                                     browser = self.restart_browser(browser)
                                                     self.click_field(field_idx=selected_region, id=1,browser=browser)  
