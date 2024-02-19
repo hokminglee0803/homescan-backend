a2b4defb7ad2e0162baee4205ec32d1c9351e215
hokminglee0803 <59437566+hokminglee0803@users.noreply.github.com>
2024-02-19 22:46:04 +0800

    Update test.py

--- app/scheduler/test.py
+++ app/scheduler/test.py
@@ -147,7 +147,7 @@ class TestScraper:
             try:
                 submit_button = browser.find_element(By.XPATH, value='//*[@id="property-valuation-search"]/div[2]/form/div/div[2]/div[1]/div/div[7]/a')
                 submit_button.click()
-                time.sleep(5)
+                time.sleep(4)
                 valuation = browser.find_element(By.XPATH, value='//*[@id="property-valuation-search"]/div[2]/form/div/div[2]/div[2]/div/div[2]/div[2]/div[2]/span').text
                 if valuation == "":
                     raise Exception
@@ -232,15 +232,12 @@ class TestScraper:
     def scrape(self, selected_region, selected_district):
         retry = 0
 
-        print(selected_region)
-        print(selected_district)
-     
         while retry < 100:
             browser = self.open_browser()
             try:
                 region_selected = self.click_field(field_idx=selected_region, id=1,
                                 browser=browser)      
-                time.sleep(2)
+                time.sleep(5)
                 district_selected = self.click_field(field_idx=selected_district, id=2,
                                 browser=browser)   
 
